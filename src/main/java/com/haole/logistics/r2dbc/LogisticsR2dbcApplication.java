7fb7a9dbb1c781f6b5887cc4d15c383c42e687ad
shengjunzhao <shengzi101@163.com>
2024-06-06 10:02:05 +0800

    add OrikaConfiguration

--- src/main/java/com/haole/logistics/r2dbc/LogisticsR2dbcApplication.java
+++ src/main/java/com/haole/logistics/r2dbc/LogisticsR2dbcApplication.java
@@ -3,7 +3,7 @@ package com.haole.logistics.r2dbc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.haole.logistics.r2dbc","net.rakugakibox.spring.boot.orika"})
+@SpringBootApplication(scanBasePackages = {"com.haole.logistics.r2dbc"})
 public class LogisticsR2dbcApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/haole/logistics/r2dbc/config/OrikaConfiguration.java
@@ -0,0 +1,16 @@
+package com.haole.logistics.r2dbc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ClassName: OrikaConfiguration
+ *
+ * @description:
+ * @author: shengjunzhao
+ * @since: 2024/6/6 9:59
+ */
+@Configuration
+@ComponentScan("net.rakugakibox.spring.boot.orika")
+public class OrikaConfiguration {
+}
