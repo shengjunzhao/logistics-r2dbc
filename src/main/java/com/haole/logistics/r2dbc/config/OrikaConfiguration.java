687752913a569c618581f0fcc0b04a0b76acbaf3
shengjunzhao <shengzi101@163.com>
2024-06-19 11:58:27 +0800

    在spring boot3.0开始不再支持spring.factories

--- src/main/java/com/haole/logistics/r2dbc/config/OrikaConfiguration.java
+++ src/main/java/com/haole/logistics/r2dbc/config/OrikaConfiguration.java
@@ -6,11 +6,12 @@ import org.springframework.context.annotation.Configuration;
 /**
  * ClassName: OrikaConfiguration
  *
- * @description:
+ * @description: 配置Orika，在spring boot3.0开始不再支持spring.factories
+ * spring.factories在2.7中已经废弃，在3.0彻底移除
  * @author: shengjunzhao
  * @since: 2024/6/6 9:59
  */
-@Configuration
-@ComponentScan("net.rakugakibox.spring.boot.orika")
+//@Configuration
+//@ComponentScan("net.rakugakibox.spring.boot.orika")
 public class OrikaConfiguration {
 }
